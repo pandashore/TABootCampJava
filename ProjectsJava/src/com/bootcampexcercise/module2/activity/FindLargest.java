b58d1300ffacc1b98f5ac9047e4bcc4151399a42
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-25 17:50:18 +0300

    Commit activity3

---
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/FindLargest.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.module2.activity;
+
+public class FindLargest {
+    public static void main(String []args) {
+        int j = 13;
+        int m = 55;
+
+        if (j > m) {
+            System.out.println("j is largest");
+        } else {
+            System.out.println("m is largest");
+        }
+
+    }
+}
