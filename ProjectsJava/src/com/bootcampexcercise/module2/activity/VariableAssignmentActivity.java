7f11f9aeaf221543ed9fa52c98c1e4690f8ab648
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-25 14:47:23 +0300

    Commit it

---
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/VariableAssignmentActivity.java
@@ -0,0 +1,10 @@
+package com.bootcampexcercise.module2.activity;
+
+public class VariableAssignmentActivity {
+
+    public static void main(String []args) {
+        int j = 3;
+        j = 5;
+        System.out.println(j);
+    }
+    }
