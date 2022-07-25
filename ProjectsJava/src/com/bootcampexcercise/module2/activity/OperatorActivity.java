bd51bb8b17a80ffa019fcb94f1b9a0b5138cd6c4
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-25 16:29:39 +0300

    commit activity2

---
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/OperatorActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module2.activity;
+
+public class OperatorActivity {
+    public static void main(String []args) {
+        int j, m;
+        j = 5;
+        m = 3;
+        int sum = j + m;
+        System.out.println("Sum of i and j is" + sum);
+    }
+}
