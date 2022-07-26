48a5caa5d9fdfae932ee577ea72ad6c16841f6c1
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-26 10:07:32 +0300

    commit activity7

---
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/PrintNumWithWhile.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.activity;
+
+public class PrintNumWithWhile {
+    public static void main(String[] args) {
+
+        // Print all odd numbers less than 100
+        int j = 1;
+        while (j < 100) {
+            System.out.println(j);
+            j = j + 2;
+        }
+
+    }
+}
