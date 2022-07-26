9df50eeccd3f955bf8133eae1f7dde25ece99659
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-26 09:42:56 +0300

    Commit activity6

--- ProjectsJava/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/AddWholeNum.java
@@ -3,8 +3,10 @@ package com.bootcampexcercise.module2.activity;
 public class AddWholeNum {
 
     public static void main(String args[]) {
+        // we want to perform sum of all whole numbers from 50 to 100//
         int sum = 0;
-        for (int j = 50; j <= 100; j++) {
+        for (int j = 50; j <= 100; j++) { //for loop repeats the action until the condition is true//
+           //initial, number of counts, increment/decrement
             sum = sum + j;
         }
         System.out.println("sum of first whole numbers is" + sum);
---
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
@@ -0,0 +1,20 @@
+package com.bootcampexcercise.module2.activity;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Multiplication Table from 11 to 20
+        for (int i = 11; i <= 20; i++) {
+            System.out.println("Multiplication Table of "+i + " Multiplication");
+            for (int j = 11; j <= 20; j++) {
+                System.out.println(j + "X" + j +" = " + i*j);
+            }
+        }
+
+
+}
+        }
+
+
+
+
+
