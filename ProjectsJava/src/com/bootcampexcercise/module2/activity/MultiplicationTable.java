df9be5e00ebd716c868fa91c3acfedf4e0725f06
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-26 12:08:34 +0300

    Commit MultiplicationTable

--- ProjectsJava/src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
+++ ProjectsJava/src/com/bootcampexcercise/module2/activity/MultiplicationTable.java
@@ -3,10 +3,10 @@ package com.bootcampexcercise.module2.activity;
 public class MultiplicationTable {
     public static void main(String[] args) {
         // Multiplication Table from 11 to 20
-        for (int i = 11; i <= 20; i++) {
+        for (int i = 11; i <= 22; i++) {
             System.out.println("Multiplication Table of "+i + " Multiplication");
-            for (int j = 11; j <= 20; j++) {
-                System.out.println(j + "X" + j +" = " + i*j);
+            for (int j = 1; j <= 10; j++) {
+                System.out.println(i + "X" + j +" = " + i*j);
             }
         }
 
