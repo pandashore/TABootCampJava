8f418ac34ecdf7120bf2018e7efbe07029098055
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-26 14:20:05 +0300

    Commit DateActivity

---
+++ ProjectsJava/src/com/bootcampexcercise/module3/activity/DateActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module3.activity;
+
+import java.util.Date;
+
+public class DateActivity {
+    public static void main(String[] args) {
+        Date todayDate = new Date(); //create instance of Date class - Date is already provided by Java// print the date
+        System.out.println(todayDate);
+    }
+
+}
