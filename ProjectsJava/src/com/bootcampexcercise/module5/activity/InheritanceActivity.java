76a623d50b24bd69b5c0b2cbecd5f1cb25a48831
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-27 10:57:57 +0300

    Commit module5 Inheritance

---
+++ ProjectsJava/src/com/bootcampexcercise/module5/activity/InheritanceActivity.java
@@ -0,0 +1,41 @@
+package com.bootcampexcercise.module5.activity;
+
+import com.bootcampexcercise.module5.activity.Person_I;
+
+public class InheritanceActivity {
+    public static void main(String[] args) {
+
+        // Create Person object p. Write code to and set name of Person as Sarah Johnson
+        // and age as 21
+Person_I p = new Person_I();
+        p.setName("Sarah Johnson");
+        p.setAge(21);
+
+        System.out.println("-----------------------------");
+        //Create Employee object e and
+        // set salary as 70000.00, title as Developer, age as 32 and name as Shawn Cun
+
+//Student_I inherit the methods and behavior from the Person_I class
+        Employee_I e = new Employee_I();
+        e.setSalary (70000);
+        e.setTitle ("Developer");
+        e.setAge (32) ;
+        e.setName ("Shawn Cun");
+
+        // Print Info using Employee object
+        System.out.println("-----------------------------");
+        System.out.println("Employee's Name is   : " + e.getName());
+        System.out.println("Employee's Age is    : " + e.getAge());
+        System.out.println("Employee's Salary is  : " + e.getSalary());
+        System.out.println("Employee's Title is : " + e.getTitle());
+
+
+
+        // Print Info using Person object
+            System.out.println("-----------------------------");
+            System.out.println("Person's Name is    : " + p.getName());
+            System.out.println("Person's Age is     : " + p.getAge());
+
+    }
+
+}
