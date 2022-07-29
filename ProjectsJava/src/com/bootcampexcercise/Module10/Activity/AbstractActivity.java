03f00071899ca917d3f6a6620e1701de0a521666
pandashore <jurgamaskeliunaite@gmail.com>
2022-07-29 16:07:01 +0300

    Update AbstractActivity.java

--- ProjectsJava/src/com/bootcampexcercise/Module10/Activity/AbstractActivity.java
+++ ProjectsJava/src/com/bootcampexcercise/Module10/Activity/AbstractActivity.java
@@ -3,9 +3,7 @@ package com.bootcampexcercise.Module10.Activity;
 import com.bootcampexcercise.Module10.Activity.Person;
 import com.bootcampexcercise.Module10.Activity.Employee;
 import com.bootcampexcercise.Module10.Activity.Students;
-import com.bootcampexcercise.module5.sample.abstraction.Employee_A;
-import com.bootcampexcercise.module5.sample.abstraction.Person_A;
-import com.bootcampexcercise.module5.sample.abstraction.Student_A;
+
 
 
 public class AbstractActivity {
@@ -24,6 +22,7 @@ public class AbstractActivity {
         eP.setAge(28);
         eP.setName("Tom");
 
+
         Employee e = new Employee();
         e.setSalary(1300);
         e.setjobTitle("JuniorSalesManager");
@@ -34,16 +33,16 @@ public class AbstractActivity {
         s.setAge(10);
         s.setName("Diana");
 
-        System.out.println("---------------------------");
-        System.out.println("Students Name            : " + pS.getName());
+        System.out.println("Hello");
+        System.out.println("Students Name         : " + pS.getName());
         System.out.println("Students Age          : " + pS.getAge());
         System.out.println("Students School Name  : " + s.getschoolName());
 
-        System.out.println("Students Name            : " + eP.getName());
-        System.out.println("Students Age          : " + eP.getAge());
+        System.out.println("Employee Name         : " + eP.getName());
+        System.out.println("Employee Age          : " + eP.getAge());
         System.out.println("Employee Salary       : " + e.getSalary());
         System.out.println("Employee Title        : " + e.getJobTitle());
-        System.out.println("Employee Company      : " + e.getCompany());
+        System.out.println("Employee Company      : " + e.getCompany();
 
     }
 }
